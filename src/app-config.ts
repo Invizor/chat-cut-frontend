8c9eb5d81eedb623f2a043f7255ba84644acd746
Invizor <okulov-nikolay@mail.ru>
2018-03-19 18:54:37 +0300

    api url change

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,5 +1,4 @@
 export class AppSettings {
-  //public static API_URL = 'https://chat-cut-server.herokuapp.com/v1';
-  public static API_URL = 'http://localhost:3000/v1';
+  public static API_URL = 'https://chat-cut-server.herokuapp.com/v1';
 }
 
