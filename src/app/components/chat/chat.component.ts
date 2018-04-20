e86fcebd5c5860d76407dea8e35abf874952d4cb
Invizor <okulov-nikolay@mail.ru>
2018-04-20 15:53:58 +0300

    test commit #2

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -224,9 +224,9 @@ export class ChatComponent implements OnInit, DoCheck, OnDestroy {
 
   addEmoji(event) {
     console.log("event", event);
-    if (event && event.emoji && event.emoji.colons) {
-      this.messageModel += event.emoji.colons;
-      this.messageModel = emojiConv.replace_colons(this.messageModel);
+    if (event && event.emoji && event.emoji.native) {
+      this.messageModel += event.emoji.native;
+      // this.messageModel = emojiConv.replace_colons(this.messageModel);
     }
     console.log("this.messageModel", this.messageModel);
     this.isOpenSmileWindow = false;
